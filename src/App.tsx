a497a0644a01cf6c609b816ae88e15920ec04f1c
websitesergio <websitesergio@gmail.com>
2026-04-18 12:59:08 +0300

    Rewrite Navbar and Hero components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Calculator from './components/Calculator';
 import Process from './components/Process';
@@ -6,6 +7,7 @@ import ContactForm from './components/ContactForm';
 export default function App() {
   return (
     <div className="bg-white min-h-screen text-slate-900 antialiased">
+      <Navbar />
       <Hero />
       <Calculator />
       <Process />
